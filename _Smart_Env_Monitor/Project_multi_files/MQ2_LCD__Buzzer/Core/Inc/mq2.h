c5e073d8726a0e5f2e664c5fc2eee0ee39cce377
alatageaditi-ship-it <alatageaditi@gmail.com>
2026-01-02 09:19:06 +0530

    _Smart_Env_Monitor

---
+++ _Smart_Env_Monitor/Project_multi_files/MQ2_LCD__Buzzer/Core/Inc/mq2.h
@@ -0,0 +1,37 @@
+/*
+ * mq2.h
+ *
+ *  Created on: Dec 30, 2025
+ *      Author: aditi
+ */
+
+#ifndef INC_MQ2_H_
+#define INC_MQ2_H_
+void MX_GPIO_Init(void);
+void MX_ADC1_Init(void);
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.h
+  * @brief          : Header for main.c file.
+  *                   This file contains the common defines of the application.
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2025 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+
+/* Define to prevent recursive inclusion -------------------------------------*/
+
+
+#ifdef __cplusplus
+
+#endif /* INC_MQ2_H_ */
